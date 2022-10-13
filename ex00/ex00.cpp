594cb339be0fc1c833502f1f256eae6081163a6b
mproveme <ruslankum2@gmail.com>
2022-10-13 19:21:00 +0300

    erunda kakaya-to

--- ex00/ex00.cpp
+++ ex00/ex00.cpp
@@ -6,11 +6,18 @@
 /*   By: mproveme <mproveme@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 18:53:58 by mproveme          #+#    #+#             */
-/*   Updated: 2022/10/13 18:57:37 by mproveme         ###   ########.fr       */
+/*   Updated: 2022/10/13 19:20:02 by mproveme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ex00.hpp"
+
 int main(int argc, char **argv)
 {
-	
+	for (int i = 1; i < argc; i++)
+	{
+		for (size_t x = 0; x < strlen(argv[i]); x++)
+        	std::cout << toupper(argv[i][x]);
+		std::cout << std::endl;
+	}
 }
--- ex00/ex00.hpp
+++ ex00/ex00.hpp
@@ -6,11 +6,12 @@
 /*   By: mproveme <mproveme@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 18:53:56 by mproveme          #+#    #+#             */
-/*   Updated: 2022/10/13 18:57:02 by mproveme         ###   ########.fr       */
+/*   Updated: 2022/10/13 19:13:13 by mproveme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EX00_HPP
 # define EX00_HPP
 # include <iostream>
+# include <cstring>
 #endif
