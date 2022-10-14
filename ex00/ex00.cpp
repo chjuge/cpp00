fa0c4c78c4d69c558d50e1293e77a4165bc2d11a
mproveme <ruslankum2@gmail.com>
2022-10-14 13:24:18 +0300

    ex00 is solved

--- ex00/ex00.cpp
+++ ex00/ex00.cpp
@@ -6,7 +6,7 @@
 /*   By: mproveme <mproveme@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 18:53:58 by mproveme          #+#    #+#             */
-/*   Updated: 2022/10/13 19:20:02 by mproveme         ###   ########.fr       */
+/*   Updated: 2022/10/14 13:23:10 by mproveme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,22 @@
 
 int main(int argc, char **argv)
 {
-	for (int i = 1; i < argc; i++)
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
 	{
-		for (size_t x = 0; x < strlen(argv[i]); x++)
-        	std::cout << toupper(argv[i][x]);
+		for (size_t i = 1, len; i < (size_t)argc; i++)
+		{
+			len = strlen(argv[i]);
+			for (size_t j = 0; j < len; j++)
+			{
+				if (isupper(argv[i][j]))
+					std::cout << (char)argv[i][j];
+				else
+					std::cout << (char)toupper(argv[i][j]);	
+			}
+			std::cout << ' ';
+		}
 		std::cout << std::endl;
 	}
 }
